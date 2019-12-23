2a69a2068610b62a36bd11a3533a819f45f1f07c
Wicked <wwicked@o2.pl>
2019-12-23 02:03:03 +0100

    Fixed typo

--- __main__.py
+++ __main__.py
@@ -1,10 +1,11 @@
 from config import Config
 from file_processor import ProcessPlugin
 from file_downloader import DownloadPlugins
+from file_converter import ConvertPlugins
 
 if __name__ == "__main__":
 	config = Config()
 
 	plugin_downloader = DownloadPlugins(config)
-	plugin_converter = CovertPlugins(config)
+	plugin_converter = ConvertPlugins(config)
 	plugin_processor = ProcessPlugin(config)
---
+++ file_converter.py
@@ -0,0 +1,3 @@
+class ConvertPlugins:
+	def __init__(self):
+		pass
