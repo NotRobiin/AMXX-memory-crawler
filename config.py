18642a2ff3f0f977847002a55f6855939398d2e3
Wicked <wwicked@o2.pl>
2019-12-23 01:08:35 +0100

    Adding gitignore, main and config

---
+++ config.py
@@ -0,0 +1,8 @@
+class Config:
+	def __init__(self):
+		self.files_path = "C:/users/robertt/desktop/memory_files"
+		self.substrings = [
+			"abcdefghijklmnopqrstu",
+			"set_user_flags",
+			"get_user_flags"
+		]
